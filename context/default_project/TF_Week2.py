4cec83afdfc343ec7ba70520e56a02a31d6c036d
Anton Matosov <anton.matosov@gmail.com>
2019-05-21 23:10:32 -0700

    couple fixes

--- context/default_project/TF_Week2.py
+++ context/default_project/TF_Week2.py
@@ -3,8 +3,11 @@ import tensorflow as tf
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
-x_train = x_train / 255.
-x_test = x_test / 255.
+
+x_train = x_train.reshape(60000, 28, 28, 1)
+x_train = x_train / 255.0
+x_test = x_test.reshape(10000, 28, 28, 1)
+x_test = x_test/255.0
 
 model = tf.keras.models.Sequential([
     # tf.keras.layers.Flatten(input_shape=(28, 28)),
